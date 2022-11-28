c3cf3ab89bc0f6ece00458327f1abc504ea8c4dc
LuisRoft <luisvelasco6541@gmail.com>
2022-11-28 11:15:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/navigatorapp/EcPlayer.kt
@@ -0,0 +1,10 @@
+package com.example.navigatorapp
+
+data class EcPlayer(
+    val playerName:String,
+    val position:String,
+    val team:String,
+    val age:Int,
+    val number:Int,
+    val photo:String
+)
---
+++ app/src/main/java/com/example/navigatorapp/EcPlayersProvider.kt
@@ -0,0 +1,95 @@
+package com.example.navigatorapp
+
+class EcPlayersProvider {
+    companion object {
+        val ecPlayersList = listOf<EcPlayer>(
+            EcPlayer(
+                "Hernan Galindez",
+                "Arquero",
+                "Aucas", 33,
+                13,
+                "https://pbs.twimg.com/media/FOqOWRcXoAAoFiX?format=jpg&name=900x900"
+            ),
+            EcPlayer(
+                "Piero Hincapie",
+                "Defensa",
+                "Bayer 04 Leverkusen",
+                20,
+                3,
+                "https://pbs.twimg.com/media/FVTTqhsWIAY1HLd?format=jpg&name=900x900"
+            ),
+            EcPlayer(
+                "Felix Torres",
+                "Defensa",
+                "Santos Laguna",
+                25,
+                2,
+                "https://vamoslatri.bolavip.com/__export/1658342929822/sites/bolavip/img/2022/07/20/torres_brasil.jpg_1159711837.jpg"
+            ),
+            EcPlayer(
+                "Pervis Estupiñán",
+                "Defensa",
+                "Brighton",
+                24,
+                7,
+                "https://www.eluniverso.com/resizer/o2nHcUCmJJGhqbmWsRQZQfF_cWk=/0x0:1400x1050/893x670/filters:quality(70)/cloudfront-us-east-1.images.arcpublishing.com/eluniverso/TWKNPND4HZETLC45OCL6HMEQ6Y.jpg"
+            ),
+            EcPlayer(
+                "Angelo Preciado",
+                "Defensa",
+                "KRC Genk",
+                24,
+                17,
+                "https://imperiofutbolec.com/wp-content/uploads/2022/09/Angelo-Preciado-Ecuador-1200x900.jpg"
+            ),
+            EcPlayer(
+                "Moises Caicedo",
+                "Mediocampista",
+                "Brighton",
+                21,
+                23,
+                "https://i2-prod.football.london/incoming/article25131606.ece/ALTERNATES/s1200c/0_GettyImages-1243558036.jpg"
+            ),
+            EcPlayer(
+                "Carlos Gruezo",
+                "Mediocampista",
+                "Augsburgo",
+                27,
+                8,
+                "https://www.ecuavisa.com/binrepository/998x601/243c0/600d600/none/11705/PAKJ/carlos-gruezo-ecuador_65955_20210628091951.jpg"
+            ),
+            EcPlayer(
+                "Gonzalo Plata",
+                "Delantero",
+                "Real Valladolid",
+                22,
+                19,
+                "https://i0.wp.com/degolagolec.com/wp-content/uploads/2022/06/gonzaloplataecu.jpg_1159711837.jpg?resize=681%2C681&ssl=1"
+            ),
+            EcPlayer(
+                "Michael Estrada",
+                "Delantero",
+                "Cruz Azul",
+                26,
+                11,
+                "https://www.ecuavisa.com/binrepository/866x596/-167c0/1200d600/none/11705/DVTL/michael-estrada-ecuador_322385_20220208115636.jpg"
+            ),
+            EcPlayer(
+                "Jeremy Sarmiento",
+                "Delantero",
+                "Brighton",
+                20,
+                10,
+                "https://www.elfutbolero.com.ec/__export/1638886663496/sites/elfutboleromx/img/2021/12/07/befunky-collage_-_2021-12-07t091526_721.jpg_2062789929.jpg"
+            ),
+            EcPlayer(
+                "Enner Valencia",
+                "Delantero",
+                "Fenerbahçe",
+                33,
+                13,
+                "https://www.diez.hn/binrepository/1356x900/75c0/1200d900/none/3014757/WOFO/enner-valencia_2993362_20221120102136.jpg"
+            )
+        )
+    }
+}
